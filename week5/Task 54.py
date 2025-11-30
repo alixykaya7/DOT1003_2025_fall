fb014e2d89008060d77d6f4ac9f85eaf5602e33d
Ali Yigit Kaya <aliyigitkaya553@gmail.com>
2025-11-30 23:34:16 +0300

    week5 commit

---
+++ week5/Task 54.py
@@ -0,0 +1,7 @@
+#Task 54
+
+sentence = ["The quick brown fox jumps over the lazy dog"]
+
+itemI = input("Which item do you want to search? ")
+
+print(f"item appeared {sentence.count(itemI)} item")
---
+++ week5/Task 55.py
@@ -0,0 +1,7 @@
+#Task 55
+
+sentenceI = [input("Enter the input to search: ")]
+
+itemI = input("Which item do you want to search? ")
+
+print(f"item appeared {sentenceI.count(itemI)} item")
