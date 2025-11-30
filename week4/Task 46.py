fbb6d5d71ac11938169c9f3bff5d651f152207cd
Ali Yigit Kaya <aliyigitkaya553@gmail.com>
2025-11-30 23:20:35 +0300

    week4 commit

---
+++ week4/Task 46.py
@@ -0,0 +1,7 @@
+#Task 46
+
+string = input("Pleas enter string: ")
+int = int(input("Pleas enter integer: "))
+
+print(string[:int])
+
---
+++ week4/Task 48.py
@@ -0,0 +1,8 @@
+#Task 48
+
+string = input("Please enter string: ")
+
+if "a" in string:
+    print("found")
+else:
+    print("not found")
